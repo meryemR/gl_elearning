f39c0abe3d0c3ae6a1d5c68c1fc2d681def46373
KhalidK7 <Khalidqqr@gmail.com>
2019-01-03 14:22:02 +0000

    small fixes (html,color..)

--- src/main/java/org/mql/models/Module.java
+++ src/main/java/org/mql/models/Module.java
@@ -29,7 +29,7 @@ public class Module {
 	@Column(name = "title")
 	private String title;
 
-	@Column(name = "description")
+	@Column(name = "description",columnDefinition="TEXT")
 	private String description;
 
 	@Column(name = "rating")
